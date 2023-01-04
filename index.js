31aa02930f65a8a6d1624764173e6d2e0b738466
Aleksandar Milutinovic <miludbulim@gmail.com>
2023-01-04 20:20:57 +0100

    updated cursorani

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ tag1.style.color = 'rgb(216, 84, 103)'
 let tag2 = document.getElementsByClassName('customTag')[1]
 tag2.innerText = '</name>'
 tag2.style.color = 'rgb(216, 84, 103)'
+
+let visibleCursor = false
+setInterval(() => {
+    if (visibleCursor){
+        document.getElementById('cursor').style.opacity = 0;
+    } else {
+        document.getElementById('cursor').style.opacity = 1;
+    }
+    visibleCursor = !visibleCursor
+}, 500);
