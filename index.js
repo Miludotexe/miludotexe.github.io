15148d0ce0a53e929c2275fead54becd089d6a08
Aleksandar Milutinovic <miludbulim@gmail.com>
2023-01-04 20:11:46 +0100

    title integrated and sidepanel changed to toppanel
    ong fuck sidepanels

---
+++ index.js
@@ -0,0 +1,7 @@
+let tag1 = document.getElementsByClassName('customTag')[0]
+tag1.innerText = '<name>'
+tag1.style.color = 'rgb(216, 84, 103)'
+
+let tag2 = document.getElementsByClassName('customTag')[1]
+tag2.innerText = '</name>'
+tag2.style.color = 'rgb(216, 84, 103)'
